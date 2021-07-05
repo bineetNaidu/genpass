a0c7398b16411a697558f6b44899310be7b62494
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-05 21:24:11 +0530

    ❇️ add constants declarations

---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*_-+=';
+
+module.exports = { alpha, numbers, symbols };
